993e9117359c6ebaede8b2fffe4fcd7d4aa9c473
Arturo López <arturo.lopez@cesjuanpablosegundo.es>
2019-11-06 11:50:07 +0100

    examen_Tema2

---
+++ DI/Examenes y entregas/Examen_Tema2_DI/src/Entrada.java
@@ -0,0 +1,26 @@
+
+import javax.swing.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Usuario DAM 2
+ */
+public class Entrada {
+    
+     public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Ventana v = new Ventana();
+                v.initGUI();
+            }
+        });
+    }
+    
+}
